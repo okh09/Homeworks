81c6822389a4e86243f9e9d96d671c87c3816eba
Oybek Khamidov <98575988+okh09@users.noreply.github.com>
2022-03-16 20:16:09 +0500

    Выполнена ДЗ №10

---
+++ src/main/java/lesson10/Apple.java
@@ -0,0 +1,13 @@
+package lesson10;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        super(1.0);
+    }
+
+    @Override
+    public String toString() {
+        return "яблоки";
+    }
+}
---
+++ src/main/java/lesson10/Orange.java
@@ -0,0 +1,13 @@
+package lesson10;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        super(1.5);
+    }
+
+    @Override
+    public String toString() {
+        return "апельсины";
+    }
+}
